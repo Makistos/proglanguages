701b3acd836f7f489db74288a3219b5dbca27bcf
Marko Poutiainen <marko.poutiainen@bittium.com>
2017-06-29 16:39:17 +0300

    Base logic but printing causes problems.

--- src/proglanguages/core.clj
+++ src/proglanguages/core.clj
@@ -2,7 +2,58 @@
   (:gen-class)
   (:require [clojure-csv.core :as csv])
   (:require [clojure.java.io :as io])
+  (:require [clojure.string :as str])
   (:use [clojure-csv.core]))
+
+(def header
+  "digraph g {
+  ranksep=.75; size =\"7.5,7.5,7.5\";
+  {
+    node [shape=plaintext, fontsize=16];\n
+  "
+   )
+
+(def footer
+  "}")
+
+
+(defn years
+  [langlist]
+  (str (clojure.string/join " -> "(map second langlist)) ";}")
+  )
+
+(defn ranks
+  [langlist]
+  (clojure.string/join "\n" (list
+                              "node [shape=box];"
+                              (map
+                                #(let [[lang year] %]
+                                   (str "{rank = same;" year ";" \" lang \" ";}"))
+                                langlist)))
+  )
+
+(defn lang-connections
+  [[language year & rest]]
+  ;;(for [lang rest]
+  (map
+    #(str language "->" % ";\n") rest))
+  ;;(map #(str language "->" % ";\n") rest))
+
+(defn connections
+  [langlist]
+  ;;(clojure.string/join "\n"
+                       (for [lang langlist] (lang-connections lang))
+  )
+
+(defn dot-file
+  [langs]
+  (clojure.string/join "\n"
+                       (list header
+                             (years langs)
+                             (ranks langs)
+                             (connections langs)
+                             footer))
+  )
 
 (defn take-csv
   "Takes file name and reads data"
@@ -13,4 +64,4 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println (take-csv "/tmp/languages.csv")))
+  (println (dot-file (take-csv "/tmp/languages.csv"))))
